f34282750168eb2c5e9bc0f85b7a47ac2005620a
Sergey <sergey.bukharov@thoughtworks.com>
2021-02-21 16:33:04 +0800

    dollar created

--- src/main/kotlin/com/stringconcat/tdd/Dollar.kt
+++ src/main/kotlin/com/stringconcat/tdd/Dollar.kt
@@ -2,4 +2,15 @@ package com.stringconcat.tdd
 
 class Dollar(
     val amount: Int
-)
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Dollar) return false
+        if (amount == other.amount) return true
+        return false
+    }
+
+    operator fun times(multiplier: Int): Dollar {
+        return Dollar(amount * multiplier)
+    }
+}
---
+++ src/test/kotlin/com/stringconcat/tdd/DollarTest.kt
@@ -0,0 +1,29 @@
+package com.stringconcat.tdd
+
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import org.junit.jupiter.api.Test
+
+internal class DollarTest {
+
+    @Test
+    fun `5 USD has amount of 5`() {
+        Dollar(5).amount shouldBe 5
+    }
+
+    @Test
+    fun `10 Usd is 10 USD`() {
+        Dollar(10) shouldBe Dollar(10)
+    }
+
+    @Test
+    fun `10 Usd is not 5 USD`() {
+        Dollar(10) shouldNotBe Dollar(5)
+    }
+
+    @Test
+    fun `5 dollar multiply 2 is 10 USD`() {
+        val ten = Dollar(5) * 2
+        ten shouldBe Dollar(10)
+    }
+}
