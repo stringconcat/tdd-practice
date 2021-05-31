5093dda0767c43fccbf21d5328b0b093ddcff06b
nikolay <nikolay.malykhin@brainway.co.il>
2021-05-31 08:59:26 +0300

    HW change problem characters

--- src/test/kotlin/com/stringconcat/tdd/MoneyTest.kt
+++ src/test/kotlin/com/stringconcat/tdd/MoneyTest.kt
@@ -17,12 +17,12 @@ internal class MoneyTest {
     }
 
     @Test
-    fun `5 dollars * 2 is 10 dollars`() {
+    fun `5 dollars multiplay 2 is 10 dollars`() {
         Money.dollar(5) * 2 shouldBe Money.dollar(10)
     }
 
     @Test
-    fun `5 dollars * 3 is 15 dollars`() {
+    fun `5 dollars multiplay 3 is 15 dollars`() {
         Money.dollar(5) * 3 shouldBe Money.dollar(15)
     }
 
@@ -37,12 +37,12 @@ internal class MoneyTest {
     }
 
     @Test
-    fun `5 franc * 2 is 10 franc`() {
+    fun `5 franc multiplay 2 is 10 franc`() {
         Money.franc(5) * 2 shouldBe Money.franc(10)
     }
 
     @Test
-    fun `5 franc * 3 is 15 franc`() {
+    fun `5 franc multiplay 3 is 15 franc`() {
         Money.franc(5) * 3 shouldBe Money.franc(15)
     }
 
@@ -65,5 +65,4 @@ internal class MoneyTest {
     fun `2 CHF + 4 USD is wallet that contains 2 CHF and 4 USD`() {
         Money.franc(2) + Money.dollar(4) shouldBe Wallet(Money.franc(2), Money.dollar(4) )
     }
-
 }
