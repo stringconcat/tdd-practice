d45bef5be757cd53002f5bc1b56f7cf04fe087d7
akoryagin256 <akoryagin256@gmail.com>
2021-10-31 16:56:36 +0300

    refactored test classes

--- src/test/kotlin/com/stringconcat/tdd/MoneyTest.kt
+++ src/test/kotlin/com/stringconcat/tdd/MoneyTest.kt
@@ -2,12 +2,9 @@ package com.stringconcat.tdd
 
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-internal class DollarTest {
-
+internal class MoneyTest {
     @Test
     fun `2 CHF is not 2 USD`() {
         Money.dollar(2) shouldNotBe Money.franc(2)
@@ -61,5 +58,45 @@ internal class DollarTest {
     @Test
     fun `5 USD to USD = 5 USD`() {
         Money.dollar(5).toDollar(1) shouldBe (Money.dollar(5))
+    }
+
+    @Test
+    fun `2 multiply 5 CHF is 10 CHF`() {
+        Money.franc(5) * 2 shouldBe Money.franc(10)
+    }
+
+    @Test
+    fun `3 multiply 5 CHF is 15 CHF`() {
+        Money.franc(5) * 3 shouldBe Money.franc(15)
+    }
+
+    @Test
+    fun `zero Francs are zero Francs`() {
+        Money.franc(0) shouldBe Money.franc(0)
+    }
+
+    @Test
+    fun `2 CHF is 2 CHF`() {
+        Money.franc(2) shouldBe Money.franc(2)
+    }
+
+    @Test
+    fun `2 CHF is not 3 CHF`() {
+        Money.franc(2) shouldNotBe Money.franc(3)
+    }
+
+    @Test
+    fun `2 CHF is not equal to null`() {
+        Money.franc(2).equals(null) shouldBe false
+    }
+
+    @Test
+    fun `2 CHF is not equal to any other object`() {
+        Money.franc(2) shouldNotBe "any string"
+    }
+
+    @Test
+    fun `2 CHF + 2 CHF is 4 CHF`() {
+        Money.franc(2) + Money.franc(2) shouldBe Money.franc(4)
     }
 }
